0cfbda8c79dd4bc6d983bca86970ded039860ded
Cao Xuân Hưng <xuanhung.cao@ved.com.vn>
2018-12-18 16:59:59 +0700

    Update

--- 00_thread_safe.py
+++ 00_thread_safe.py
@@ -2,15 +2,16 @@ import threading
 import utils
 
 THREAD_COUNT = 10
-INC_COUNT = 1000
+INC_COUNT = 100
 
 value = 0
+lock = threading.Lock()
 
 def inc(n):
 	global value
 	for _ in range(n):
-		value += 1
-
+		with lock:
+			value += 1
 
 for i in range(THREAD_COUNT):
 	t = threading.Thread(target=inc, args=(INC_COUNT,))
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import threading
+import dis
 
 def join_other_threads():
 	main_thread = threading.currentThread()
 	for t in threading.enumerate():
 		if t is not main_thread:
 			t.join()
+
+def disassenble(func):
+	dis.dis(func)
